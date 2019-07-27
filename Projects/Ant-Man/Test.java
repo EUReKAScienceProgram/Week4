fbf3fd57a22fd4fb5119d25f6cf36352a8cee893
EUReKACamper11 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-27 16:29:28 -0700

    addd files to ant man - Reed

---
+++ Projects/Ant-Man/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+public static void main(String[] args){     
+    System.out.println("ME AND THE BOYS AT THE FOOD STAND AT AREA 51 YEEEEEEEET");
+int myNumber = 123456789;  
+System.out.println(myNumber);
+System
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

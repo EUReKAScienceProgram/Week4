56e8300c87f1e7c63be41195e7efe32baccea5f5
EUReKACamper7 <51757403+EUReKACamper@users.noreply.github.com>
2019-07-27 14:29:56 -0700

    added files to scarlett witch - Reed

---
+++ Projects/ScarletWitch/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main(String[] args){
+        System.out.println("EUReKA!");
+
+int myNumber =17;
+System.out.println(myNumBER);
+
+String thisIsMyString = "MARSHMELLOW";
+           
+        
+        
+        
+        
+
+
+    }
+
+}

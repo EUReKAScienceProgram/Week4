2369c2987debc2613f18c24c87f2e4451218ee69
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-27 14:51:52 -0700

    added files to Thor - Reed

---
+++ Projects/Thor/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main (String[] args){
+        System.out.println("Lil Nas X-Old Town Road");
+        System.out.println("Update");
+        System.out.println("Lil Nas X-Old Town Road feat.Billy Ray Cyrus");
+        System.out.println("Update");
+        System.out.println("Lil Nas X & Bill Ray Cyrus-Old Town Road Diplo Remix");
+        System.out.println("Update");
+        System.out.println("Lil Nas X & Bill Ray Cyrus-Old Town Road feat.Young Thug & Mason Ramsey");
+        
+        int myNumber = 666;
+        System.out.println(myNumber);
+        String Area51sep20 = "Yeet";
+        System.out.println(Area51sep20);
+
+    }
+}

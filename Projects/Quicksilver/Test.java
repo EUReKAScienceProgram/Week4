eaa24c6950f631986d432854f82516cdf6d02293
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-27 14:39:28 -0700

    added files to quicksilver - Reed

---
+++ Projects/Quicksilver/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+public static void main (String[] args){
+    System.out.println("Hello World");
+}
+
+int myNumber =8:
+System.out.println(myNumber)
+String.out thisIsMyString =  "SHOUTOUT TO TEACH CAMP";
+System.out.printlin(thisIsMyString);
+
+}
+
+
+
+
+
+
+
